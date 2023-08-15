0b6e3a5dd75a93839ce27893b77d212ec44ad373
kri-code <56919961+kri-code@users.noreply.github.com>
2023-08-15 19:17:00 +0200

    Update mpt.py

--- mpt.py
+++ mpt.py
@@ -28,21 +28,6 @@ from transformers import AutoTokenizer
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
 
 import torch
-from transformers import StoppingCriteria, StoppingCriteriaList
-
-# mtp-7b is trained to add "<|endoftext|>" at the end of generations
-stop_token_ids = tokenizer.convert_tokens_to_ids(["<|endoftext|>"])
-
-# define custom stopping criteria object
-class StopOnTokens(StoppingCriteria):
-    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
-        for stop_id in stop_token_ids:
-            if input_ids[0][-1] == stop_id:
-                return True
-        return False
-
-stopping_criteria = StoppingCriteriaList([StopOnTokens()])
-
 from transformers import pipeline
 
 pipe = pipeline('text-generation',
