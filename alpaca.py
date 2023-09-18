44b99e5a9dea8edcc64822d83dcd37cc7de943e0
kri-code <56919961+kri-code@users.noreply.github.com>
2023-09-18 21:38:44 +0200

    Add files via upload

---
+++ alpaca.py
@@ -0,0 +1,14 @@
+import transformers
+
+alpaca_model = transformers.AutoModelForCausalLM.from_pretrained("alpaca_weights")
+alpaca_tokenizer = transformers.AutoTokenizer.from_pretrained("alpaca_weights")
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+pipeline = pipeline(model=model_name, torch_dtype=torch.bfloat16, trust_remote_code=True, device=device)
+
+dialog = "Tell me about alpacas"
+
+generate_text = pipeline(dialog)
+
+print(generate_text[0]["generated_text"])
