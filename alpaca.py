9cca662321027850c665e9df00bd9b0588d080a2
kri-code <56919961+kri-code@users.noreply.github.com>
2023-09-18 22:00:54 +0200

    Update alpaca.py

--- alpaca.py
+++ alpaca.py
@@ -7,7 +7,7 @@ alpaca_tokenizer = transformers.AutoTokenizer.from_pretrained("../alpaca_weights
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-pipeline = pipeline(model=model_name, torch_dtype=torch.bfloat16, trust_remote_code=True, device=device)
+pipeline = pipeline(model=alpaca_model, torch_dtype=torch.bfloat16, trust_remote_code=True, device=device)
 
 dialog = "Tell me about alpacas"
 
