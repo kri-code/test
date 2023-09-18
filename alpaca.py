f867781225b1280aa1fa8516577d32b12a48dc1c
kri-code <56919961+kri-code@users.noreply.github.com>
2023-09-18 22:14:40 +0200

    Update alpaca.py

--- alpaca.py
+++ alpaca.py
@@ -3,9 +3,11 @@ import torch
 from transformers import pipeline
 
 alpaca_model = transformers.AutoModelForCausalLM.from_pretrained("../alpaca_weights")
+alpaca_tokenizer = transformers.AutoTokenizer.from_pretrained("../alpaca_weights")
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-pipeline = pipeline(task='text-generation', model=alpaca_model, torch_dtype=torch.bfloat16, trust_remote_code=True, device=device)
+pipeline = pipeline(task='text-generation', model=alpaca_model, tokenizer= alpaca_tokenizer, torch_dtype=torch.bfloat16, trust_remote_code=True, device=device)
 
 dialog = "Tell me about alpacas"
 
