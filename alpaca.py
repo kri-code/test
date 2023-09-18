51fd0650ba4bb7ca7e7b60508b1f782e3b1b25ae
kri-code <56919961+kri-code@users.noreply.github.com>
2023-09-18 22:41:40 +0200

    Update alpaca.py

--- alpaca.py
+++ alpaca.py
@@ -7,7 +7,7 @@ alpaca_tokenizer = transformers.AutoTokenizer.from_pretrained("../alpaca_weights
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-pipeline = pipeline(task= "text-generation", model=alpaca_model, tokenizer= alpaca_tokenizer, max_new_tokens=50,device=device, do_sample=False)
+pipeline = pipeline(task= "text-generation", model=alpaca_model, tokenizer= alpaca_tokenizer, max_new_tokens=100,device=device, do_sample=False)
 
 dialog = "Tell me about elephants"
 
