efb439f95a1a1e9e0df0c876063b5d31063e4984
kri-code <56919961+kri-code@users.noreply.github.com>
2023-09-19 20:10:51 +0200

    Update alpaca.py

--- alpaca.py
+++ alpaca.py
@@ -2,7 +2,7 @@ import transformers
 import torch
 from transformers import pipeline
 
-alpaca_model = transformers.AutoModelForCausalLM.from_pretrained("../alpaca_weights")
+alpaca_model = transformers.AutoModelForCausalLM.from_pretrained("../alpaca_weights", device_map="auto")
 alpaca_tokenizer = transformers.AutoTokenizer.from_pretrained("../alpaca_weights")
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -11,8 +11,8 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 input_text = (
             "Below is an instruction that describes a task. "
-            "Write a response that appropriately completes the request.\r\n\r\n"
-            "### Instruction:\r\nTell me about Alpacas.\r\n\r\n### Response:"
+            "Give advice or suggestions in response to a mental health-related question\r\n\r\n"
+            "### Instruction:\r\n"I'm going through some things with my feelings and myself. I barely sleep and I do nothing but think about how I'm worthless and how I shouldn't be here. I've never tried or contemplated suicide. I've always wanted to fix my issues, but I never get around to it. How can I change my feeling of being worthless to everyone?"\r\n\r\n### Response:"
         )
 inputs = alpaca_tokenizer(input_text, return_tensors="pt")
 out = alpaca_model.generate(inputs=inputs.input_ids, max_new_tokens=100)
