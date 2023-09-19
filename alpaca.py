7bd2f0603f1afbd3d2bea3f56eb63a4a68697a78
kri-code <56919961+kri-code@users.noreply.github.com>
2023-09-19 20:12:58 +0200

    Update alpaca.py

--- alpaca.py
+++ alpaca.py
@@ -12,7 +12,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 input_text = (
             "Below is an instruction that describes a task. "
             "Give advice or suggestions in response to a mental health-related question\r\n\r\n"
-            "### Instruction:\r\n"I'm going through some things with my feelings and myself. I barely sleep and I do nothing but think about how I'm worthless and how I shouldn't be here. I've never tried or contemplated suicide. I've always wanted to fix my issues, but I never get around to it. How can I change my feeling of being worthless to everyone?"\r\n\r\n### Response:"
+            "### Instruction:\r\n I'm going through some things with my feelings and myself. I barely sleep and I do nothing but think about how I'm worthless and how I shouldn't be here. I've never tried or contemplated suicide. I've always wanted to fix my issues, but I never get around to it. How can I change my feeling of being worthless to everyone? \r\n\r\n### Response:"
         )
 inputs = alpaca_tokenizer(input_text, return_tensors="pt")
 out = alpaca_model.generate(inputs=inputs.input_ids, max_new_tokens=100)
