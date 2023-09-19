d03e5df41e91be5372f571722528db5cb3b58d3e
kri-code <56919961+kri-code@users.noreply.github.com>
2023-09-19 15:21:41 +0200

    Update alpaca.py

--- alpaca.py
+++ alpaca.py
@@ -7,9 +7,19 @@ alpaca_tokenizer = transformers.AutoTokenizer.from_pretrained("../alpaca_weights
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-pipeline = pipeline(task= "text-generation", model=alpaca_model, tokenizer= alpaca_tokenizer, max_new_tokens=100,device=device, do_sample=False)
+#pipeline = pipeline(task= "text-generation", model=alpaca_model, tokenizer= alpaca_tokenizer, max_new_tokens=100,device=device, do_sample=False)
 
-dialog = "Tell me about elephants"
+input_text = (
+            "Below is an instruction that describes a task. "
+            "Write a response that appropriately completes the request.\r\n\r\n"
+            "### Instruction:\r\nTell me about Alpacas.\r\n\r\n### Response:"
+        )
+inputs = alpaca_tokenizer(input_text, return_tensors="pt")
+out = alpaca_model.generate(inputs=inputs.input_ids, max_new_tokens=100)
+output_text = alpaca_tokenizer.batch_decode(out, skip_special_tokens=True)[0]
+output_text = output_text[len(input_text) :]
+print(f"Input: {input_text}\nCompletion: {output_text}")
+
 
 generate_text = pipeline(dialog)
 
