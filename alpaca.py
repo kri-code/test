afafe499934300f9cec1f68071190b3d4de25588
kri-code <56919961+kri-code@users.noreply.github.com>
2023-09-20 18:29:33 +0200

    Update alpaca.py

--- alpaca.py
+++ alpaca.py
@@ -15,7 +15,7 @@ input_text = (
             "### Instruction:\r\n I'm going through some things with my feelings and myself. I barely sleep and I do nothing but think about how I'm worthless and how I shouldn't be here. I've never tried or contemplated suicide. I've always wanted to fix my issues, but I never get around to it. How can I change my feeling of being worthless to everyone? \r\n\r\n### Response:"
         )
 inputs = alpaca_tokenizer(input_text, return_tensors="pt")
-out = alpaca_model.generate(inputs=inputs.input_ids, max_new_tokens=100)
+out = alpaca_model.generate(inputs=inputs.input_ids, max_new_tokens=256)
 output_text = alpaca_tokenizer.batch_decode(out, skip_special_tokens=True)[0]
 output_text = output_text[len(input_text) :]
 print(f"Input: {input_text}\nCompletion: {output_text}")
