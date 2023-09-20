2d7603e6f4ed8b21d34fbe3c7359d2e8c7f62ab8
kri-code <56919961+kri-code@users.noreply.github.com>
2023-09-20 19:06:51 +0200

    Update alpaca.py

--- alpaca.py
+++ alpaca.py
@@ -5,7 +5,8 @@ from transformers import pipeline
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 alpaca_model = transformers.AutoModelForCausalLM.from_pretrained("../alpaca_weights", device_map="auto")
-alpaca_model.load_adapter(experiments)
+alpaca_model.add_adapter(experiments, adapter_name="adapter_1")
+alpaca_model.set_adapter("adapter_1")
 alpaca_tokenizer = transformers.AutoTokenizer.from_pretrained("../alpaca_weights")
 
 #pipeline = pipeline(task= "text-generation", model=alpaca_model, tokenizer= alpaca_tokenizer, max_new_tokens=100,device=device, do_sample=False)
